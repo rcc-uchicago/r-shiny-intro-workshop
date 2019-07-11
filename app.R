d3c7f388a76dc2fafb39849178d87fe1f41e039c
Nicholas Marchio <namarchio@gmail.com>
2019-07-11 09:55:12 -0500

    Update app.R

--- app.R
+++ app.R
@@ -1,13 +1,13 @@
 # Demo of k-means clustering app
-# Created by Nicholas Marchio
-# Modified from RStudio script here https://www.dropbox.com/s/rjt6g3ctdqvihat/shiny-quickstart-1.zip
+# Nicholas Marchio 07/2019
+# Based on RStudio materials here https://www.dropbox.com/s/rjt6g3ctdqvihat/shiny-quickstart-1.zip
 # Code here: https://github.com/rcc-uchicago/r-shiny-intro-workshop
 # Slides here: https://docs.google.com/presentation/d/1fuUIlfagMGkDzUlRQxjIImjY7mxgqqmS335MYzTqNbc/edit
 
 # Global environment ------------------------------------------------------
 # Put all code that runs on startup in the Global i.e. libraries, functions, pre-loaded data, etc.
 
-library(shiny) # Load packages
+library(shiny)
 library(tidyr)
 library(ggplot2)
 library(dplyr)
@@ -30,7 +30,7 @@ ui <- fluidPage(
     radioButtons(inputId = "labels", label = "Cluster labels", choices = c("k-means" = "kmeans_label","Actual" = "actual_label")) # Radio button toggle widget
   ),
   mainPanel(
-    plotOutput(outputId = 'xyplot') 
+    plotOutput(outputId = 'xyplot') # Render plot output
   )
 )
 
@@ -38,28 +38,21 @@ ui <- fluidPage(
 
 # Construct the server object
 server <- function(input, output, session) {
-
+  
   # Reactives that subsets down to data selected from the input widget
-  selectedData <- reactive({
-    iris[, c(input$xcol, input$ycol)] # Dataframe of selected data in input widget
-  })
-  selectedX <- reactive({
-    iris[, c(input$xcol)] # Dataframe of selected data in input widget
-  })
-  selectedY <- reactive({
-    iris[, c(input$ycol)] # Dataframe of selected data in input widget
-  })
-
+  selectedData <- reactive({iris[, c(input$xcol, input$ycol)]})
+  selectedX <- reactive({iris[, c(input$xcol)]})
+  selectedY <- reactive({iris[, c(input$ycol)]})
+  
   # Reactive that runs k-means clustering function on SelectedData() reactive
   clusters <- reactive({
-    kmeans(x = selectedData(), # Reactive object representing selected columns based on user input
-           centers = input$clusters) # Number of clusters k in input widget
+    kmeans(x = selectedData(), centers = input$clusters) # Run k-means clustering
   })
   
   # Reactive value that contains the legend's cluster groupings
   grouping <- reactiveValues()
-
-  # Observe event to adjust the number of clusters and update the radio button
+  
+  # Observe event to adjust the number of clusters and update the radio button to reflect selection
   observeEvent(input$clusters, {
     if(input$labels=="kmeans_label" | (input$labels=="actual_label" & input$clusters != 3)) { 
       grouping$data <- as.factor(clusters()$cluster) # Assign k-means clusters to data
@@ -67,17 +60,15 @@ server <- function(input, output, session) {
     updateRadioButtons(session, inputId = 'labels', selected = if(length(unique(grouping$data)) == 3 & input$labels=="actual_label") {"actual_label"} else {"kmeans_label"} ) # Update radio button
   })
   
-  # Observe event to change between 'k-means' and 'Actual' and update the slider
+  # Observe event to change between 'k-means' and 'Actual' radio buttons and update the slider if 'Actual' is selected
   observeEvent(input$labels, {
     if(input$labels=="actual_label") {grouping$data <- iris$Species} # Assign actual species clusters to data
     else { grouping$data <- as.factor(clusters()$cluster) } # Assign k-means clusters to data
     updateSliderInput(session, inputId = 'clusters', value = length(unique(grouping$data)) ) # Update value to correspond to the number of clusters in the legend
   })
   
-  # Render a plot of the input reactive data as an output
+  # Render a plot of the reactive data as an output object
   output$xyplot <- renderPlot({
-    par(mar = c(5.1, 4.1, 0, 1)) # Graph position
-    
     # Create X-Y scatter plot
     ggplot(data=iris) + # Raw iris dataset
       geom_point(aes_string(x=selectedX(), # Reactive for x input widget
